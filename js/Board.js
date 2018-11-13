e89c1d601704fc687d2945e848a0764cf288945b
vlad_osadchyi <v.osadchyi@gmail.com>
2018-11-13 11:38:00 -0500

    Project compleate

--- js/Board.js
+++ js/Board.js
@@ -13,16 +13,27 @@ class Board {
         const spaces = [];
         
 		for (let x = 0; x < this.columns; x++) {
-			const column = [];
+			const col = [];
 			
 			for (let y = 0; y < this.rows; y++) {
 				const space = new Space(x, y);
-				column.push(space);
+				col.push(space);
 			}
 			
-			spaces.push(column);
+			spaces.push(col);
 		}
         
         return spaces;
     }
+    
+    /** 
+     * Draws associated SVG spaces for all game spaces.
+     */
+	drawHTMLBoard() {
+        for (let column of this.spaces) {
+            for (let space of column) {
+                space.drawSVGSpace();
+            }
+        }
+	} 
 }
